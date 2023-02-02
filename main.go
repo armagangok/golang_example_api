c3a4ebf4d21c94c5bbec0ed7b2aac0001cf2c94b
Armağan Gök <1armagangok@gmail.com>
2023-02-02 22:38:38 +0300

    first api on fire

--- main.go
+++ main.go
@@ -1,34 +1,33 @@
 package main
 
-
 import (
-	"errors"
 	"net/http"
 
+	"errors"
+
 	"github.com/gin-gonic/gin"
-	
 )
 
-type Book struct {
+type book struct {
 	ID       string `json:"id"`
 	Title    string `json:"title"`
 	Author   string `json:"author"`
 	Quantity int    `json:"quantity"`
 }
 
-var books = []Book{
+var books = []book{
 	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
 	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
 
-func fetchBooks(c *gin.Context) {
+func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
 }
 
 func bookById(c *gin.Context) {
 	id := c.Param("id")
-	book, err := fetchBookById(id)
+	book, err := getBookById(id)
 
 	if err != nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
@@ -38,7 +37,50 @@ func bookById(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, book)
 }
 
-func fetchBookById(id string) (*Book, error) {
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available."})
+		return
+	}
+
+	book.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func returnBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter."})
+		return
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found."})
+		return
+	}
+
+	book.Quantity += 1
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func getBookById(id string) (*book, error) {
 	for i, b := range books {
 		if b.ID == id {
 			return &books[i], nil
@@ -48,38 +90,23 @@ func fetchBookById(id string) (*Book, error) {
 	return nil, errors.New("book not found")
 }
 
-// func fetchBookByID(id string)(*Book, error) {
-// 	for i,b := range books {
-// 		b.ID == id {
-// 			 return &books[i], nil
-// 		}
+func createBook(c *gin.Context) {
+	var newBook book
 
-// 	}
-
-// 	return nil, errors.New("book not found")
-// }
-
-func storeBooks(c *gin.Context) {
-	var newBook Book
-	if errorOccured := c.BindJSON(&newBook); errorOccured != nil {
+	if err := c.BindJSON(&newBook); err != nil {
 		return
 	}
 
 	books = append(books, newBook)
-
-	c.IndentedJSON(http.StatusCreated, books)
+	c.IndentedJSON(http.StatusCreated, newBook)
 }
-
-func router() {
-	router := gin.Default()
-	router.GET("/books", fetchBooks)
-	router.GET("/books/:id", bookById)
-	router.POST("/books", storeBooks)
-	router.Run("localhost:8080")
-
-}
-
 
 func main() {
-	router()
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.GET("/books/:id", bookById)
+	router.POST("/books", createBook)
+	router.PATCH("/checkout", checkoutBook)
+	router.PATCH("/return", returnBook)
+	router.Run("localhost:8080")
 }
